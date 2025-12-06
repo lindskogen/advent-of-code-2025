38ec5e28e82dbc50cfb367a9dd583fe62aa2d186
Johan Lindskogen <johan.lindskogen@gmail.com>
2025-12-06 21:31:13 +0100

    Add template

---
+++ _template/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+const example_input =
+    \\987654321111111
+;
+
+const inputfile = @embedFile("input");
+
+fn run_part1(_: anytype) !usize {
+
+    return 0;
+}
+
+fn run_part2(_: anytype) !usize {
+    return 0;
+}
+
+pub fn main() !void {
+    const p1 = try run_part1(inputfile);
+    const p2 = try run_part2(inputfile);
+
+    std.debug.print("Part 1: {d}\n", .{p1});
+    std.debug.print("Part 2: {d}\n", .{p2});
+}
+
+const test_input = "";
+
+test "input1" {
+    try std.testing.expectEqual(1227775554, run_part1(test_input));
+}
+
+test "input2" {
+    try std.testing.expectEqual(4174379265, run_part2(test_input));
+}
+
+test "star 1" {
+    try std.testing.expectEqual(44854383294, run_part1(inputfile));
+}
+
+test "star 2" {
+    try std.testing.expectEqual(55647141923, run_part2(inputfile));
+}
