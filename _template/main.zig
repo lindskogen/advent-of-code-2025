8ddc2c4b6400f560c54a06271ca3ea8a0314032f
Johan Lindskogen <johan.lindskogen@gmail.com>
2025-12-08 22:06:38 +0100

    Add allocators to template

--- _template/main.zig
+++ _template/main.zig
@@ -6,18 +6,21 @@ const example_input =
 
 const inputfile = @embedFile("input");
 
-fn run_part1(_: anytype) !usize {
+fn run_part1(_: anytype, _: std.mem.Allocator) !usize {
 
     return 0;
 }
 
-fn run_part2(_: anytype) !usize {
+fn run_part2(_: anytype, _: std.mem.Allocator) !usize {
     return 0;
 }
 
 pub fn main() !void {
-    const p1 = try run_part1(inputfile);
-    const p2 = try run_part2(inputfile);
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_allocator.deinit();
+    const alloc = arena_allocator.allocator();
+    const p1 = try run_part1(inputfile, alloc);
+    const p2 = try run_part2(inputfile, alloc);
 
     std.debug.print("Part 1: {d}\n", .{p1});
     std.debug.print("Part 2: {d}\n", .{p2});
@@ -25,17 +28,29 @@ pub fn main() !void {
 
 
 test "input1" {
-    try std.testing.expectEqual(1227775554, run_part1(example_input));
+    var arena_allocator = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena_allocator.deinit();
+    const alloc = arena_allocator.allocator();
+    try std.testing.expectEqual(1227775554, run_part1(example_input, alloc));
 }
 
 test "input2" {
-    try std.testing.expectEqual(4174379265, run_part2(example_input));
+    var arena_allocator = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena_allocator.deinit();
+    const alloc = arena_allocator.allocator();
+    try std.testing.expectEqual(4174379265, run_part2(example_input, alloc));
 }
 
 test "star1" {
-    try std.testing.expectEqual(44854383294, run_part1(inputfile));
+    var arena_allocator = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena_allocator.deinit();
+    const alloc = arena_allocator.allocator();
+    try std.testing.expectEqual(44854383294, run_part1(inputfile, alloc));
 }
 
 test "star2" {
-    try std.testing.expectEqual(55647141923, run_part2(inputfile));
+    var arena_allocator = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena_allocator.deinit();
+    const alloc = arena_allocator.allocator();
+    try std.testing.expectEqual(55647141923, run_part2(inputfile, alloc));
 }
